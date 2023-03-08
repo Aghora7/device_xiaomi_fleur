50730f2aa28f3667cb1f331745972d1499cc1b64
aghora7 <ukikieran@gmail.com>
2023-03-08 17:26:07 +0530

    fleur: Initial Tree based  on A12 13.0.4
    Signed-off-by: aghora7 <ukikieran@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The PixelExperience Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_fleur.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_fleur-user \
+    aosp_fleur-userdebug \
+    aosp_fleur-eng
